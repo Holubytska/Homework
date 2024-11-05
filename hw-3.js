dc552422cbdad91888b8c392393c608b80270354
Holubytska <oksanagolubickay@gmal.com>
2024-11-05 17:49:27 +0200

    Задание 1.

---
+++ hw-3.js
@@ -0,0 +1,11 @@
+// Задание 1
+
+let password = 'password';
+let correctPassword = prompt('Введите пароль');
+
+if (correctPassword === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
+
