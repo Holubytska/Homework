7c8dbf5cf8606e51b185bf12f53470da78b99567
Holubytska <oksanagolubickay@gmal.com>
2024-11-05 19:15:41 +0200

    Задание 2.

--- hw-3.js
+++ hw-3.js
@@ -1,19 +1,10 @@
-// Задание 1
-
-// let password = 'password';
-// let correctPassword = prompt('Введите пароль');
-
-// if (correctPassword === password) {
-//     console.log('Пароль введен верно');
-// } else {
-//     console.log('Пароль введен неправильно');
-// }
-
 // Задание 2
 
-let c = 2;
+let c = Number(prompt('Введите число'));
 if (c >= 0 && c <= 10) {
     console.log('Верно');
 } else {
-   console.log('Неверно'); 
+   console.log('Неверно');
 }
+
+
