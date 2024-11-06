6422d5e3270074c3882679e0494e2c204025305c
Holubytska <oksanagolubickay@gmal.com>
2024-11-06 05:46:33 +0200

    Задание 5.

--- hw-3.js
+++ hw-3.js
@@ -1,8 +1,27 @@
-// Задание 4
+// Задание 5
 
-let a = '2';
-let b = '3';
-
-alert(+a + +b);
-
-
+let monthNumber = String(prompt('Введите номер месяца'));
+switch (monthNumber) {
+    case '12':
+    case '1':
+    case '2':
+        console.log('Зима');
+        break;
+    case '3':
+    case '4':
+    case '5':
+        console.log('Весна');
+        break;
+    case '6':
+    case '7':
+    case '8':
+        console.log('Лето');
+        break;
+    case '9':
+    case '10':
+    case '11':
+        console.log('Осень');
+        break;
+    default: console.log('Месяца с таким номером не существует');
+        break;
+}
