cf42645a31408fadc9f41857b9c1f5116347fe64
Holubytska <oksanagolubickay@gmal.com>
2024-11-06 11:39:25 +0200

    Добавила дополнительные задания 2 и 3.

--- hw-3.js
+++ hw-3.js
@@ -1,18 +1,37 @@
 // Дополнительные задания:
 // Задание 1
 
-let anyNumber = Number(prompt("Пожалуйста, введите любое число"));
-let result = Number(anyNumber);
+// let anyNumber = Number(prompt("Пожалуйста, введите любое число"));
+// let result = Number(anyNumber);
 
-if (isNaN(result)) {
-    console.log('Не число');
-} else { 
-    console.log('Число');
-}
+// if (isNaN(result)) {
+//     console.log('Не число');
+// } else { 
+//     console.log('Число');
+// }
 
-if (result % 2 === 0) {
-      alert('Чётное');
+// if (result % 2 === 0) {
+//       alert('Чётное');
+// } else {
+//       alert('Нечетное');
+// }
+   
+// Задание 2
+
+let clientOS = 0;
+
+if (clientOS === 0) {
+    alert('Установите версию приложения для iOS по ссылке');
 } else {
-      alert('Нечетное');
+    alert('Установите версию приложения для Android по ссылке');
 }
-    
+
+// Задание 3
+
+let clientDeviceYear = 2015;
+
+if (clientDeviceYear <=2015) {
+    alert('Установите облегченную версию приложения для iOS по ссылке')
+} else {
+    alert('Установите версию приложения для iOS по ссылке')
+}
