e66b1545d93e68dde6de309cdecae2772bd927b5
Holubytska <oksanagolubickay@gmal.com>
2024-11-09 13:35:27 +0200

    Chenged file index.html

--- hw-1.js
+++ hw-1.js
@@ -6,12 +6,12 @@
 // alert(a);
 
 // Задание 2
-let date = 2007;
-alert(date);
+// let date = 2007;
+// alert(date);
 
-// Задание 3
-let name = "Брендан Эйх";
-alert(name);
+// // Задание 3
+// let name = "Брендан Эйх";
+// alert(name);
 
 // Задание 4
 // let a = 10;
@@ -31,29 +31,29 @@ alert(name);
 
 // Задание 5
 
-let result = 2 ** 5;
-alert(result);
+// let result = 2 ** 5;
+// alert(result);
 
 // Задание 6
-let a = 9;
-let b = 2;
-let remainder = a % b;
-alert(remainder);
+// let a = 9;
+// let b = 2;
+// let remainder = a % b;
+// alert(remainder);
 
-// Задание 7
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num = num ++;
-num = num --;
-alert(num);
+// // Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num = num ++;
+// num = num --;
+// alert(num);
 
-// Задание 8
+// // Задание 8
 
-let age = Number(prompt('Сколько вам лет'));
-alert(age);
+// let age = Number(prompt('Сколько вам лет'));
+// alert(age);
 
 // Задание 9
 
@@ -70,18 +70,18 @@ alert(age);
 
 // Дополнительное задание
 
-let hiddenNumber = Number(prompt('Загадайте любое число'));
+// let hiddenNumber = Number(prompt('Загадайте любое число'));
 
-let doubleNumber = hiddenNumber * 2;
-alert(doubleNumber);
+// let doubleNumber = hiddenNumber * 2;
+// alert(doubleNumber);
 
-let biggerNumber = doubleNumber + 10;
-alert(biggerNumber);
+// let biggerNumber = doubleNumber + 10;
+// alert(biggerNumber);
 
-let dividedNumber = biggerNumber / 2;
-alert(dividedNumber);
+// let dividedNumber = biggerNumber / 2;
+// alert(dividedNumber);
 
-let originalNumber = dividedNumber - hiddenNumber;
-alert(originalNumber);
+// let originalNumber = dividedNumber - hiddenNumber;
+// alert(originalNumber);
 
-alert(`Вы загадали ${originalNumber}`);
+// alert(`Вы загадали ${originalNumber}`);
---
+++ hw-3.js
@@ -0,0 +1,146 @@
+// // Задание 1
+
+// for (let i = 0; i < 2; i++) {
+//     console.log('Привет!');
+// }
+
+
+// // // Задание 2
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 6);
+ 
+// // // или
+
+// for (let i = 1; i <=5 ; i++) {
+//    console.log(i);
+// }
+
+// // // Задание 3
+
+// for (let i = 7; i <= 22; i++) {
+//     console.log(i);
+// }
+
+// // // Задание 4
+
+
+// const obj = {
+//     Nik : '200',
+//     Vasya : '300',
+//     Pety : '400'
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} - ${obj[key]} dollars`);
+//  }
+
+// //  Задание 5
+
+// let n = 1000;
+// let num = 0;
+
+// while (n >= 50) {
+//     n /= 2;
+//     console.log(n);
+//     num++;
+// }
+
+// console.log(`Количество циклов ${num}`);
+
+// // Задание 6
+
+// let dayFriday = 2;
+// let allDay = 31;
+
+// for (let i = dayFriday; i <= allDay; i += 7) {
+//     console.log(`Сегодня пятница ${i}. Необходимо подготовить отчет.`);
+// }
+
+// // Дополнительные задания
+
+// // Задание 1
+
+// let k = 100;
+// let iterations = 0;
+
+// while (k >= 0) {
+//     k -= 7;
+//     console.log(k);
+//     iterations++;
+// }
+
+// console.log(`Количество циклов ${iterations}`);
+
+
+// // Задание 2
+
+// let months = ["January-1", "February-2", "March-3", "April-4", "May-5", "June-6", "July-7", "August-8", "September-9", "October-10", "November-11", "December-12"];
+
+// for (let i = 0; i < 1; i++) {
+//     console.log(months[0]);
+// }
+
+// for (let i = 1; i < 2; i++) {
+//     console.log(months[1]);
+// }
+// for (let i = 2; i < 3; i++) {
+//     console.log(months[2]);
+// }
+
+// for (let i = 3; i < 4; i++) {
+//     console.log(months[3]);
+// }
+
+// for (let i = 4; i < 5; i++) {
+//     console.log(months[4]);
+// }
+
+// for (let i = 5; i < 6; i++) {
+//     console.log(months[5]);
+// }
+// for (let i = 6; i < 7; i++) {
+//     console.log(months[6]);
+// }
+// for (let i = 7; i < 8; i++) {
+//     console.log(months[7]);
+// }
+// for (let i = 8; i < 9; i++) {
+//     console.log(months[8]);
+// }
+// for (let i = 9; i < 10; i++) {
+//     console.log(months[9]);
+// }
+// for (let i = 10; i < 11; i++) {
+//     console.log(months[10]);
+// }
+// for (let i = 11; i < 12; i++) {
+//     console.log(months[11]);
+// }
+
+
+
+// // Задание 3
+
+// const book = {
+//     BookTitle: 'Murder on the Orient Express.',
+//     Author: 'Agatha Christie',
+//     YearOfPublication: '1934',
+//     Genre : 'Detective'
+//  }
+
+// for (const key in book) {
+//     console.log(`${key} - ${book[key]}`);
+// }
+
+// // Задание 4
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 1; i < 2; i++) {
+// 	console.log(arr[i]);
+// }
+
